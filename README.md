655b765f71fb37df9dab951db6675b95c991dff0
Dzov <amelie.haladjian@gmail.com>
2017-08-01 20:02:49 +0200

    feat(*): first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+MeetYourDev
+===========
+
+A Symfony project created on July 31, 2017, 2:28 pm.
---
+++ src/MYD/PlatformBundle/MYDPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MYD\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MYDPlatformBundle extends Bundle
+{
+}
