1a742e71180ab3b64659f691f5d5fc78842dc74d
Dzov <amelie.haladjian@gmail.com>
2017-08-08 21:09:54 +0200

    feat(doctrine): add advert, categories and image entities

--- src/MYD/PlatformBundle/Controller/AdvertController.php
+++ src/MYD/PlatformBundle/Controller/AdvertController.php
@@ -7,6 +7,11 @@ namespace MYD\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use MYD\PlatformBundle\Entity\Advert;
+use MYD\PlatformBundle\Entity\Image;
+use MYD\PlatformBundle\Entity\Application;
+use MYD\PlatformBundle\Entity\Categories;
+
 
 class AdvertController extends Controller
 {
@@ -22,51 +27,9 @@ class AdvertController extends Controller
 
     // Ici, on récupérera la liste des annonces, puis on la passera au template
 
-    $listAdverts = array(
-      array(
-        'title'   => 'Recherche développpeur Symfony',
-        'id'      => 1,
-        'author'  => 'Alexandre',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Mission de webmaster',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Offre de stage webdesigner',
-        'id'      => 3,
-        'author'  => 'Mathieu',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime()), 
-      array(
-        'title'   => 'Mission de webmaster',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Lorem ipsum',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Lorem ipsum',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Lorem ipsum',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-        'date'    => new \Datetime())
-    );
-
+    $listAdverts = $this->getDoctrine()
+    ->getRepository('MYDPlatformBundle:Advert')
+    ->findAll(); 
 
     // Mais pour l'instant, on ne fait qu'appeler le template
     return $this->render('MYDPlatformBundle:Advert:index.html.twig', array('listAdverts' => $listAdverts ));
@@ -74,38 +37,86 @@ class AdvertController extends Controller
 
   public function viewAction($id)
   {
+
+    $em = $this->getDoctrine()->getManager();
     // Ici, on récupérera l'annonce correspondante à l'id $id
-    $advert = array(
-      'title'   => 'Recherche développpeur Symfony2',
-      'id'      => $id,
-      'author'  => 'Alexandre',
-      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-      'date'    => new \Datetime()
-    );
+    $advert = $em
+    ->getRepository('MYDPlatformBundle:Advert')
+    ->find($id)
+    ;
+
+    // $advert est donc une instance de MYD\PlatformBundle\Entity\Advert
+    // ou null si l'id $id  n'existe pas, d'où ce if :
+    if (null === $advert) {
+      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+    $applications = $em
+    ->getRepository('MYDPlatformBundle:Application')
+    ->findBy(array('advert' => $advert))
+    ;
 
     return $this->render('MYDPlatformBundle:Advert:view.html.twig', array(
-      'advert' => $advert
-    ));
+      'advert' => $advert, 
+      'applications' => $applications
+      ));
   }
 
   public function addAction(Request $request)
   {
-    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+    // Création de l'entité
+    $advert = new Advert();
+    $advert->setTitle('Super projet !');
+    $advert->setAuthor('Marion');
+    $advert->setContent("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe recusandae molestiae, dicta accusantium, veniam nisi deserunt minima facilis soluta tempora voluptatem hic. Minima error inventore, animi, atque officia saepe laudantium.");
+    // On peut ne pas définir ni la date ni la publication,
+    // car ces attributs sont définis automatiquement dans le constructeur
 
-    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    $image = new Image(); 
+    $image->setUrl('https://www.w3schools.com/css/img_fjords.jpg'); 
+    $image->setAlt('Fjords');
+
+    $advert->setImage($image); 
+
+    // Création d'une première candidature
+    $application1 = new Application();
+    $application1->setAuthor('Marine');
+    $application1->setContent("J'ai toutes les qualités requises.");
+
+    // Création d'une deuxième candidature par exemple
+    $application2 = new Application();
+    $application2->setAuthor('Pierre');
+    $application2->setContent("Je suis très motivé.");
+
+    // On lie les candidatures à l'annonce
+    $application1->setAdvert($advert);
+    $application2->setAdvert($advert);
+
+    // On récupère l'EntityManager
+    $em = $this->getDoctrine()->getManager();
+
+    // Étape 1 : On « persiste » l'entité
+    $em->persist($advert);
+
+    // Étape 1 ter : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
+    // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
+    $em->persist($application1);
+    $em->persist($application2);
+
+
+    // Étape 2 : On « flush » tout ce qui a été persisté avant
+    $em->flush();
+
+    // Reste de la méthode qu'on avait déjà écrit
     if ($request->isMethod('POST')) {
-      // Ici, on s'occupera de la création et de la gestion du formulaire
-
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
       // Puis on redirige vers la page de visualisation de cettte annonce
-      return $this->redirectToRoute('myd_platform_view', array('id' => 5));
+      return $this->redirectToRoute('myd_platform_view', array('id' => $advert->getId()));
     }
 
     // Si on n'est pas en POST, alors on affiche le formulaire
-
-    return $this->render('MYDPlatformBundle:Advert:add.html.twig'
-    );
+    return $this->render('MYDPlatformBundle:Advert:add.html.twig', array('advert' => $advert));
   }
 
   public function editAction($id, Request $request)
@@ -116,27 +127,58 @@ class AdvertController extends Controller
     if ($request->isMethod('POST')) {
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
 
-      return $this->redirectToRoute('myd_platform_view', array('id' => 5));
+      return $this->redirectToRoute('myd_platform_view', array('id' => $id));
     }
 
-    $advert = array(
-      'title'   => 'Recherche développpeur Symfony',
-      'id'      => $id,
-      'author'  => 'Alexandre',
-      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sunt tempora, iure consequatur hic molestiae laboriosam pariatur facere. Laboriosam nihil aliquam labore odit consequatur, eveniet, illo hic pariatur veniam tempore!',
-      'date'    => new \Datetime()
-    );
+    $em = $this->getDoctrine()->getManager();
+
+    // On récupère l'annonce $id
+    $advert = $em->getRepository('MYDPlatformBundle:Advert')->find($id);
+
+    if (null === $advert) {
+      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+    // La méthode findAll retourne toutes les catégories de la base de données
+    $listCategories = $em->getRepository('MYDPlatformBundle:Category')->findAll();
+
+    // On boucle sur les catégories pour les lier à l'annonce
+    foreach ($listCategories as $category) {
+      $advert->addCategory($category);
+    }
+
+    // Pour persister le changement dans la relation, il faut persister l'entité propriétaire
+    // Ici, Advert est le propriétaire, donc inutile de la persister car on l'a récupérée depuis Doctrine
+
+    // Étape 2 : On déclenche l'enregistrement
+    $em->flush();
 
     return $this->render('MYDPlatformBundle:Advert:edit.html.twig', array(
       'advert' => $advert
-    ));
+      ));
   }
 
   public function deleteAction($id)
   {
-    // Ici, on récupérera l'annonce correspondant à $id
+    $em = $this->getDoctrine()->getManager();
 
-    // Ici, on gérera la suppression de l'annonce en question
+    // On récupère l'annonce $id
+    $advert = $em->getRepository('OCPlatformBundle:Advert')->find($id);
+
+    if (null === $advert) {
+      throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+    // On boucle sur les catégories de l'annonce pour les supprimer
+    foreach ($advert->getCategories() as $category) {
+      $advert->removeCategory($category);
+    }
+
+    // Pour persister le changement dans la relation, il faut persister l'entité propriétaire
+    // Ici, Advert est le propriétaire, donc inutile de la persister car on l'a récupérée depuis Doctrine
+
+    // On déclenche la modification
+    $em->flush();
 
     return $this->render('MYDPlatformBundle:Advert:delete.html.twig');
   }
@@ -145,16 +187,14 @@ class AdvertController extends Controller
   {
     // On fixe en dur une liste ici, bien entendu par la suite
     // on la récupérera depuis la BDD !
-    $listAdverts = array(
-      array('id' => 2, 'title' => 'Recherche développeur Symfony'),
-      array('id' => 5, 'title' => 'Mission de webmaster'),
-      array('id' => 9, 'title' => 'Offre de stage webdesigner')
-    );
+    $listAdverts = $this->getDoctrine()
+    ->getRepository('MYDPlatformBundle:Advert')
+    ->findBy(array(), ['id' => 'DESC' ], $limit, null); 
 
     return $this->render('MYDPlatformBundle:Advert:menu.html.twig', array(
       // Tout l'intérêt est ici : le contrôleur passe
       // les variables nécessaires au template !
       'listAdverts' => $listAdverts
-    ));
+      ));
   }
 }
--- src/MYD/PlatformBundle/Entity/User.php
+++ src/MYD/PlatformBundle/Entity/User.php
@@ -18,6 +18,14 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    /**
+     * @ORM\ManyToMany(targetEntity="MYD\PlatformBundle\Entity\Group")
+     * @ORM\JoinTable(name="fos_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
 
     public function __construct()
     {
